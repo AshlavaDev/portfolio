7424e63e5c97460823a2d5605b5a6c24c3f95aa8
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-28 16:52:13 -0600

    add intro page and make responsive to mobile

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,11 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
 
 type Props = {};
 
 export default function Intro({}: Props) {
   return (
-    <section>
-      <h1 className='font-alegreya'>Ashley Morgan</h1>
+    <section id='intro' className='flex flex-col justify-center items-center min-h-screen'>
+      <div className="h-64 w-64 relative bg-burgundy rounded-full">
+        <Image
+          src='/images/profile1.png'
+          alt='A picture of a non-binary person, Ashley Morgan, smiling'
+          fill
+          objectFit='cover'
+          className='rounded-full'
+        />
+      </div>
+      <h1 className='font-offside md:text-6xl text-4xl py-4 md:py-6'>Ashley Morgan</h1>
+      <div className='flex flex-col md:flex-row gap-y-5 items-center justify-between gap-x-6 font-alegreya text-2xl md:text-4xl text-burgundy'>
+        <div className='flex flex-col items-center gap-y-4'>
+          <h3 className='text-4xl md:text-6xl'>Web Developer</h3>
+          <div className='flex gap-x-4'>
+            <Link href='https://github.com/AshlavaDev'>
+              <SiGithub />
+            </Link>
+            <Link href='https://www.linkedin.com/in/ashley-morgan-wbdv/'>
+              <SiLinkedin />
+            </Link>
+          </div>
+        </div>
+        <div className='flex flex-col justify-start'>
+          <h4>Fullstack</h4>
+          <h4>Frontend Accessibility</h4>
+        </div>
+      </div>
     </section>
   )
 };
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,19 +13,19 @@ export default function NavBar({}: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className='font-alegreya bg-burgundy min-w-full text-cream px-4 md:px-8 py-2 md:py-4 flex flex-col md:flex-row gap-y-4 md:items-center md:justify-between fixed w-full z-20 top-0 left-0'>
+    <header className='font-alegreya bg-burgundy min-w-full text-cream px-4 md:px-8 py-2 md:py-4 flex flex-col justify-center md:flex-row gap-y-4 md:items-center md:justify-between fixed w-full z-20 top-0 left-0'>
       <div className='flex justify-between items-center space-x-2'>
         {/*TODO: Add Logo */}
         <h2 className='font-offside text-2xl hidden md:block'>Ashley Morgan</h2>
       </div>
-      <button className='block md:hidden' onClick={() => setIsOpen(!isOpen)}>
+      <button className='block md:hidden ' onClick={() => setIsOpen(!isOpen)}>
         <span className="sr-only">Open main menu</span>
         <SlMenu />
       </button>
       <nav className={`w-full block md:flex md:w-auto ${isOpen ? "block" : "hidden"}`}>
         <ul className='text-md md:text-lg md:flex md:flex-row md:gap-x-10 gap-y-5 md:items-center'>
           <li>
-            <Link href='' className='navLink'>
+            <Link href='#intro' className='navLink'>
               Home
             </Link>
           </li>
