c993ca8573079f4f4551c7e66f0805322d6ea876
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-28 14:26:42 -0600

    set up colours and fonts with test to ensure functional

---
+++ components/Intro.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Intro({}: Props) {
+  return (
+    <section>
+      <h1 className='font-offside'>Ashley Morgan</h1>
+    </section>
+  )
+};
---
+++ components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {}
+
+export default function NavBar({}: Props) {
+  return (
+    <header className='font-alegreya bg-burgundy min-w-full h-10 '></header>
+  )
+};
