31d99becb38e0f76687c1cd838af251d25480a76
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-29 15:01:45 -0600

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Analytics } from '@vercel/analytics/react';
 export const metadata = {
   title: 'Ashley Morgan',
   description: 'Portfolio for Ashley Morgan',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
