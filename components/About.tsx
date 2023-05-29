6c725ad2751fc5f55dca93abd4f368f110c1475a
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-28 18:47:02 -0600

    add background colour

--- components/About.tsx
+++ components/About.tsx
@@ -4,8 +4,13 @@ type Props = {}
 
 export default function About({}: Props) {
   return (
-    <section id='about' className='min-h-screen'>
-
+    <section id='about' className='min-h-screen flex flex-col md:flex-row justify-evenly'>
+      <div>
+        <h2 className='font-offside text-burgundy text-4xl md:text-6xl'>About Me</h2>
+      </div>
+      <div>
+        <h2 className='font-offside text-4xl md:text-6xl'>Skills</h2>
+      </div>
     </section>
   )
 };
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 export default function Projects({}: Props) {
   return (
-    <section id='projects' className='min-h-screen flex items-center'>
-      <h2 className='text-burgundy font-offside text-4xl md:text-8xl px-10'>Projects</h2>
+    <section id='projects' className='min-h-screen flex items-center bg-burgundy'>
+      <h2 className='text-cream font-offside text-4xl md:text-8xl px-10'>Projects</h2>
     </section>
   )
 };
