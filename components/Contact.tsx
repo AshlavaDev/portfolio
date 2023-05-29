905aaa7ea56318290240affb723b683ba6f223e4
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-29 14:47:25 -0600

    footer padding issue fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,7 +8,7 @@ type Props = {}
 
 export default function Contact({}: Props) {
   return (
-    <section id='contact' className='min-h-screen w-screen bg-burgundy text-cream relative'>
+    <section id='contact' className='min-h-screen w-screen bg-burgundy text-cream relative flex'>
       <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center gap-x-20 mb-4'>
         <div className='pt-24 px-6 text-xl md:text-2xl flex flex-col items-center md:items-end gap-y-6'>
           <h2 className='font-offside text-4xl md:text-8xl'>Contact Me</h2>
@@ -25,7 +25,7 @@ export default function Contact({}: Props) {
             </Link>
           </span>
         </div>
-        <div className='container flex flex-col items-center md:items-start md:pt-36 px-6 pt-10 min-w-full'>
+        <div className='container flex flex-col items-center md:items-start md:pt-36 px-6 pt-10 pb-10 min-w-full'>
           <p className='text-2xl md:text-4xl'>Or contact me through this form</p>
           <ContactForm />
         </div>
