41625be43a0ea7f52c71f30eaa941107deb6ea2c
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-05-28 18:33:12 -0600

    add about and contact to site

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Intro from "@/components/Intro";
 import NavBar from "@/components/NavBar";
 import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-cream font-alegreya ">
+    <main className="flex min-h-screen flex-col bg-cream font-alegreya">
       <NavBar />
       <Intro />
       <Projects />
+      <About />
+      <Contact />
     </main>
   )
 };
