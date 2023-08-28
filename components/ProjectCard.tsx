233f60e9f72f857e9569ecb67d5c8bba68242dd3
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-08-28 09:32:50 -0600

    reverse project card colours

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -20,7 +20,7 @@ export default function ProjectCard({project, projectName, image, briefDescripti
       pathname: `/projects/${project}`,
       query: { project: project} 
     }} >
-      <div className='border border-solid border-cream hover:bg-cream rounded text-cream hover:text-burgundy w-80 h-96 p-1 flex flex-col items-center justify-evenly'>
+      <div className='border border-solid border-cream bg-cream hover:bg-burgundy rounded hover:text-cream text-burgundy w-80 h-96 p-1 flex flex-col items-center justify-evenly'>
         <h4 className='text-xl md:text-2xl font-semibold'>{projectName}</h4>
         <Image src={image.src} alt={image.alt} width={288} height={240} />
         <p className='text-lg md:text-xl text-center'>{briefDescription}</p>
