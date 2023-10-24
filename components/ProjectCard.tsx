e3973fce0340dcf4d41498f7d444acfbf4b84981
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-10-24 13:56:38 -0600

    make projects section more responsive to medium screens

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -16,11 +16,17 @@ type Props = {
 export default function ProjectCard({project, projectName, image, briefDescription, keywords}: Props) {
   return (
     <Link href={`/projects/${project}`} >
-      <div className='border border-solid border-cream bg-cream hover:bg-burgundy rounded hover:text-cream text-burgundy w-80 h-96 p-1 flex flex-col items-center justify-evenly'>
+      <div className='border border-solid border-cream bg-cream hover:bg-burgundy rounded hover:text-cream text-burgundy w-80 h-96 p-1 flex flex-col items-center justify-evenly md:w-[50rem] md:h-52 md:p-4'>
         <h4 className='text-xl md:text-2xl font-semibold'>{projectName}</h4>
-        <Image src={image.src} alt={image.alt} width={288} height={240} />
-        <p className='text-lg md:text-xl text-center'>{briefDescription}</p>
-        <p className='text-lg md:text-xl text-center'>{keywords}</p>
+        <div className='flex flex-col md:flex-row gap-2'>
+          <div className='md:w-1/3'>
+            <Image src={image.src} alt={image.alt} width={288} height={240} className='object-cover w-full h-full'/>
+          </div>
+          <div className='flex flex-col gap-2 md:w-2/3'>
+            <p className='text-lg md:text-xl text-center'>{briefDescription}</p>
+            <p className='text-lg md:text-xl text-center'>{keywords}</p>
+          </div>
+        </div>
       </div>
     </Link>
   )
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,9 +5,9 @@ import data from '../data/projects/data.json';
 export default function Projects() {
 
   return (
-    <section id='projects' className='min-h-screen flex flex-col md:flex-row items-center md:justify-between bg-burgundy'>
-      <h2 className='text-cream font-offside text-4xl md:text-8xl px-20 pt-24 md:pt-0 pb-12 md:pb-0'>Projects</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-4 md:pr-20 pb-6 md:pb-0'>
+    <section id='projects' className='min-h-screen flex flex-col xl:flex-row items-center justify-between xl:justify-center bg-burgundy md:pt-6 pb-4'>
+      <h2 className='text-cream font-offside text-4xl md:text-8xl px-20 pt-24 md:pt-0 pb-12 xl:pb-0'>Projects</h2>
+      <div className='grid grid-cols-1 justify-items-center gap-4 pb-6 md:pb-0'>
         {data.map((project, index) => (
           <ProjectCard key={index} project={project.project} projectName={project.projectName} image={project.images[0]} briefDescription={project.briefDescription} keywords={project.keywords} />
         ))}
