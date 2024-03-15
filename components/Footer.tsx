7c3ab1df13e0ffc9e52b96ab7950f11c38496bd2
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2024-03-14 23:01:13 -0600

    update year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='bg-cream text-burgundy border-t border-burgundy px-4 py-2 w-full flex justify-evenly md:text-lg'>
-      <span>© 2023 Ashley Morgan</span>
+      <span>© 2024 Ashley Morgan</span>
       <span>Deployed with Vercel</span>
     </footer>
   )
