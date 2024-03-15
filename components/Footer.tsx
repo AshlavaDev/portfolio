f39f774dabbb27a237a30f44d85f2e5508550f17
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2024-03-14 23:00:50 -0600

    fix footer margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='bg-cream text-burgundy border-t border-burgundy px-4 py-2 mt-4 w-full flex justify-evenly md:text-lg'>
+    <footer className='bg-cream text-burgundy border-t border-burgundy px-4 py-2 w-full flex justify-evenly md:text-lg'>
       <span>© 2023 Ashley Morgan</span>
       <span>Deployed with Vercel</span>
     </footer>
